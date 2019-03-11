a7eabd0b6775f7bf706d164b6cc94a048bd9ed12
Keavon Chambers <keavon@keavon.com>
2019-03-11 04:10:05 -0700

    Add shortcut to show/hide all 2D colliders with the "C" key

--- Assets/Editor/CustomEditorShortcuts.cs
+++ Assets/Editor/CustomEditorShortcuts.cs
@@ -3,9 +3,15 @@ using UnityEditor;
 
 public static class CustomEditorShortcuts
 {
-    [MenuItem("Edit/Redo %#Z")]
-    static void Redo()
-    {
-        Undo.PerformRedo();
-    }
+	[MenuItem("Edit/Redo %#Z")]
+	static void Redo()
+	{
+		Undo.PerformRedo();
+	}
+
+	[MenuItem("Edit/Show|Hide All 2D Colliders _c")]
+	static void ShowHideAll2DColliders()
+	{
+		Physics2D.alwaysShowColliders = !Physics2D.alwaysShowColliders;
+	}
 }
