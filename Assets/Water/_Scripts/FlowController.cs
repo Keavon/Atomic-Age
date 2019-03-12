eca838fc7847fe92fb482c51fc531924fe4feb17
Vanessa Lopez <vanessa@Vanessas-MacBook-Pro.local>
2019-03-12 11:23:33 -0700

    Added buoyancy/drag to boxes falling through water

--- Assets/Water/_Scripts/FlowController.cs
+++ Assets/Water/_Scripts/FlowController.cs
@@ -4,20 +4,14 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class FlowController : MonoBehaviour {
-    //public GameObject Water;
 
-    // Use this for initialization
-    void Start () {
-    }
-	
-	// Update is called once per frame
-	void Update () {
-    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        transform.parent.GetComponent<FlowBehavior>().Overflow();
-        //Water.GetComponent<FlowBehavior>().Overflow();
+        if (other.CompareTag("Player") || (other.CompareTag("Crate")))
+        {
+            transform.parent.GetComponent<FlowBehavior>().Overflow();
+        }
     }
 
 
