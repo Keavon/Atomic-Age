49b72a696b9d0d696f944de2ec8fd60301dc82e4
Trace Rainbolt <tracerainbolt@gmail.com>
2019-03-11 17:37:49 -0700

    Added ventillation room #9

---
+++ Assets/Props (Interactive Stationary)/Ventillation Fan/FanBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanBehavior : MonoBehaviour {
+
+    public float fanForce;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.AddForce(Vector2.up * fanForce);
+        }
+    }
+}
