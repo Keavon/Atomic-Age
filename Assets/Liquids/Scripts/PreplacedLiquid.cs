2ec9f4df50f1944b11b71f8fa6f3ffcb19fa5205
Keavon Chambers <keavon@keavon.com>
2019-03-14 02:21:50 -0700

    Make preplaced liquid get placed in its Liquids gameobject instead of the root

--- Assets/Liquids/Scripts/PreplacedLiquid.cs
+++ Assets/Liquids/Scripts/PreplacedLiquid.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class PreplacedLiquid : MonoBehaviour {
 	//Manny Jimenez, needed this for being able to preset liquid in levels
 
 	public Fluid fluid;
-    public GameObject liquidPrefab;
-    // public SpriteRenderer dripsRenderer;
+	public GameObject liquidPrefab;
 
 	// Use this for initialization
-	void Awake() {
+	void Start() {
 		GameObject liquid = Instantiate(liquidPrefab, new Vector3(0, 0, -1), Quaternion.identity);
-		liquid.GetComponent<LiquidBehavior>().PlaceLiquid(transform.position, fluid,  false);
+		liquid.transform.parent = gameObject.transform.parent;
+		liquid.GetComponent<LiquidBehavior>().PlaceLiquid(transform.position, fluid, false);
 	}
 }
