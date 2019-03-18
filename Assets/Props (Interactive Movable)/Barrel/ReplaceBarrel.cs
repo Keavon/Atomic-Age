103474dd3d0dc00c9e3b97c30160b4a774b488e9
Trace Rainbolt <tracerainbolt@gmail.com>
2019-03-18 16:37:23 -0700

    Finished my levels, added launch bunker.

---
+++ Assets/Props (Interactive Movable)/Barrel/ReplaceBarrel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceBarrel : MonoBehaviour {
+
+    public float spawnX;
+    public float spawnY;
+
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("PropReplace"))
+        {
+            transform.localPosition = new Vector3(spawnX, spawnY, 0);
+        }
+    }
+}
---
+++ Assets/Props (Interactive Movable)/Boxes/_Scripts/BoxReplace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxReplace : MonoBehaviour {
+
+    public float spawnX;
+    public float spawnY;
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("PropReplace"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            transform.localPosition = new Vector3(spawnX, spawnY, 0);
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
